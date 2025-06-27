0cfb54ef23afa0a46ad7c985993a782e2ea1e0f7
musa-rafay <musa@rafay.co>
2025-06-27 12:57:04 -0500

    Update gen_stable_builds.py to keep track of version number

--- ci/gen_stable_builds.py
+++ ci/gen_stable_builds.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 import yaml
-with open('stable-builds.yml','w') as f:
-    yaml.dump({'alpha':'alpha:1.0','bravo':'bravo:1.0'}, f)
-print('stable-builds.yml written')
+import os
+
+def bump(version):
+    major, minor = map(int, version.split('.'))
+    return f"{major}.{minor + 1}"
+
+filename = 'stable-builds.yml'
+
+# Load existing versions or start fresh
+if os.path.exists(filename):
+    with open(filename) as f:
+        builds = yaml.safe_load(f) or {}
+else:
+    builds = {}
+
+# Default to 1.0 if not set, otherwise bump
+builds['alpha'] = bump(builds.get('alpha', '1.0'))
+builds['bravo'] = bump(builds.get('bravo', '1.0'))
+
+# Save updated versions
+with open(filename, 'w') as f:
+    yaml.dump(builds, f)
+
+print("stable-builds.yml updated:", builds)
